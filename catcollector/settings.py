f3b6d87c0a3e1c66a88ce2ab0376a6d04fa38865
time2fishman <adam_h_martinez@hotmail.com>
2022-08-23 11:28:52 -0600

    hide secret

--- catcollector/settings.py
+++ catcollector/settings.py
@@ -19,13 +19,11 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7#!j066dns1+ne!mma$_&9=1n&=sd1iq100v*1xn*yw0f18u(%'
-
+SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True if os.getenv('MODE') == 'dev' else False
 
